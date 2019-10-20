ba74c84c5aa139d27ce557d6789221fef4e25757
flare - Jamie <flare2399@gmail.com>
2019-10-20 19:56:49 +0100

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="r6statsapi",
-    version="0.0.1",
-    author="flare",
-    author_email="flare2399@gmail.com",
-    description="Wrapper for R6 Stats API",
-    long_description="Async Wrapper for R6 Stats API",
-    long_description_content_type="text/markdown",
-    url="https://github.com/flaree/r6statsapi",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
