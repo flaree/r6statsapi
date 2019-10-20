e2f85f32898bf23beaabef290d99f2dcedc1caac
flare - Jamie <flare2399@gmail.com>
2019-10-20 16:46:24 +0100

    flaree

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="flare",
+    version="0.0.1",
+    author="flare",
+    author_email="flare2399@gmail.com",
+    description="Wrapper for R6 Stats API",
+    long_description="Async Wrapper for R6 Stats API",
+    long_description_content_type="text/markdown",
+    url="https://github.com/flaree/r6statsapi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
